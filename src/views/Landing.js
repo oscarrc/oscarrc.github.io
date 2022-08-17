f2b326e9dfd511615ea4e1d25a89006e095c04e7
Oscar <oscarrc.web@gmail.com>
2022-08-17 17:03:45 +0200

    [MOD] BV logo to public

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -1,13 +1,12 @@
 import './Landing.css';
 
 import React from 'react';
-import logo from '../assets/logo.svg';
 
 const Landing = () => {
   return (
     <div className="Landing">
       <header className="Landing-header">
-        <img src={logo} className="Landing-logo" alt="logo" />
+        <img src="/logo.svg" className="Landing-logo" alt="logo" />
         <p>
           Edit <code>src/views/Landing.js</code> and save to reload.
         </p>
