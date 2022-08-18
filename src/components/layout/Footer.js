49633375ffc7f5f1afd1269633c48f9005bb3f89
Oscar <oscarrc.web@gmail.com>
2022-08-18 13:34:31 +0200

    [MDO] Footer responsiveness

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,14 +5,14 @@ import social from "../../config/social";
 const Footer = ({ terminal, setTerminal }) => {
     return (
         <footer>
-            <div className="flex flex-col items-center gap-4 w-8 fixed bottom-4 left-2">
-                <div className="h-32 m-0 divider divider-horizontal self-center text-xs">
-                    <span className="rotate-180 writing-mode-vertical">SOCIAL</span>   
+            <div className="flex flex-col items-center gap-4 md:w-8 fixed bottom-4 left-2">
+                <div className="w-full md:h-32 m-0 divider md:divider-horizontal self-center text-xs">
+                    <span className="md:rotate-180 md:writing-mode-vertical">SOCIAL</span>   
                 </div>
-                <ul className="flex flex-col justify-center gap-4">
+                <ul className="flex md:flex-col justify-center gap-4">
                     {
                         Object.keys(social).map(key =>
-                            <li key={key} className="tooltip tooltip-right" data-tip={social[key].label}>
+                            <li key={key} className="tooltip md:tooltip-right" data-tip={social[key].label}>
                                 <a href={social[key].url} target="_blank" rel="noopener noreferrer">{ social[key].icon }</a>
                             </li>
                         )
