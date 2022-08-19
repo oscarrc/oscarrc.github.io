643d1020a9caec7763aae40ae5af2611e410ea5c
Oscar <oscarrc.web@gmail.com>
2022-08-19 10:27:09 +0200

    [MOD] Handle shift

--- src/components/partials/Keyboard.js
+++ src/components/partials/Keyboard.js
@@ -11,6 +11,11 @@ const Keyboard = ({ isDeleting, pressed, speed = 100 }) => {
         return pressed.toLowerCase()
     }
 
+    const isShift = () => {
+        if(isDeleting || pressed === " ") return false;
+        if(pressed.toUpperCase() === pressed ) return "lshift";
+    }
+
     useEffect(() => {
         if(pressed) setPress(true);
 
@@ -26,7 +31,7 @@ const Keyboard = ({ isDeleting, pressed, speed = 100 }) => {
     return (
         <div className="grid grid-cols-qwerty-responsive md:grid-cols-qwerty grid-rows-qwerty-responsive md:grid-rows-qwerty gap-y-[0.75vw] md:gap-y-1">
             {
-                keys.map((key, index) => <div key={ index } className={`mr-1 rounded-sm transition-all ${ isPressed() === key.key && press ? 'bg-primary' : 'bg-secondary' } ${key.span}`}></div>)
+                keys.map((key, index) => <div key={ index } className={`mr-1 rounded-sm transition-all ${ (isPressed() === key.key || isShift() === key.key ) && press ? 'bg-primary' : 'bg-secondary' } ${key.span}`}></div>)
             }
         </div>
     )
