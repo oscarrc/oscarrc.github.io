e34947ea1d852d9d2a69047d52ca683924687d72
Oscar <oscarrc.web@gmail.com>
2022-09-02 19:37:37 +0200

    [FIX] Code break line

--- src/components/terminal/index.js
+++ src/components/terminal/index.js
@@ -66,10 +66,14 @@ const Terminal = ({ isOpen, setOpen }) => {
     return (
         <div ref={termRef} onClick={ () => inputRef.current.focus() } className={`mockup-code overflow-y-scroll fixed mx-4 sm:mx-16 my-16 transition-all inset-0 duration-250 ease-in-out origin-bottom-right ${isOpen ? 'scale-1 translate-y-0 translate-x-0' : 'scale-0 translate-y-8 translate-x-8'} shadow-lg`}>
             {
-                lines.map( (line, index) => <pre className={ line.classes } key={index} data-prefix={line.prefix || ""}><code>{ line.text }</code></pre> )
+                lines.map( (line, index) => 
+                    <pre className={ line.classes } key={index} data-prefix={line.prefix || ""}>
+                        <code className="inline-block whitespace-normal	max-w-full break-all pr-6">{ line.text }</code>
+                    </pre>
+                )
             }
             <pre className="mt-4 mb-2" data-prefix="$">
-                <code>
+                <code className="inline-block whitespace-normal	max-w-full break-all pr-6">
                     <input aria-label="command prompt" ref={ inputRef } className="input active:outline-transparent focus:outline-transparent bg-transparent border-none p-0 h-6 rounded-none" type="text" />
                 </code>
             </pre>
