c7bf0839f781b0c68003a2abd279c73bbd15d750
Oscar <oscarrc.web@gmail.com>
2022-09-02 17:43:02 +0200

    [ADD] Views

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Suspense, lazy } from "react";
 import Layout from "./components/layout";
 
 const Landing = lazy(() => import('./views/Landing'));
+const Projects = lazy(() => import('./views/projects'));
+const Blog = lazy(() => import('./views/blog'));
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Suspense>
         <Routes>
           <Route path="/" element={ <Landing /> } />
+          <Route path="/projects" element={ <Projects /> } />
+          <Route path="/blog" element={ <Blog /> } />
         </Routes>
       </Suspense>
     </Layout>
