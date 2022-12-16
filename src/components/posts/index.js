362b8eadcc07e617f1c40f375924cc3b08ca1143
Oscar <oscarrc.web@gmail.com>
2022-12-16 14:23:11 +0100

    [ADD] Infinite scroll

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,4 +1,4 @@
-import { Suspense, useMemo } from "react";
+import { Suspense, useEffect, useMemo } from "react";
 
 import PostCard from "./PostCard";
 import config from "../../config/github"
@@ -7,6 +7,7 @@ import { useNavigate, Await } from "react-router-dom";
 import { getFiles, getMedia } from "../../lib/github"
 import { parse } from "../../lib/mdx";
 import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
 
 const fetchPosts = async (page, limit) => {
     const files = await getFiles(config.user, config.repo, "gh-posts", page, limit);
@@ -29,11 +30,17 @@ const postsLoader = (queryClient, page = 0, limit = 9) => async () => {
 }
 
 const Posts = ({ limit = 9, infinite}) => {
-    const { data:posts } = useInfiniteQuery(["posts"], ({pageParam = 0}) => fetchPosts(pageParam, limit), {
+    const { 
+        isFetchingNextPage, 
+        fetchNextPage,
+        data:posts 
+    } = useInfiniteQuery(["posts"], ({pageParam = 0}) => fetchPosts(pageParam, limit), {
         getNextPageParam: (lastPage) =>  lastPage.pages?.next ?? undefined,
         getPreviousPageParam: (firstPage) => firstPage.pages?.prev ?? undefined, 
     })
     
+    const { ref: next, inView: loadNext } = useInView();
+
     const navigate = useNavigate();
 
     const children = useMemo(() => {
@@ -52,11 +59,16 @@ const Posts = ({ limit = 9, infinite}) => {
         )
     }, [navigate, posts?.pages])
 
+    useEffect(() => {
+        if (loadNext && !isFetchingNextPage) fetchNextPage();
+    }, [loadNext, isFetchingNextPage, fetchNextPage])
+
     return (
         <div className="flex w-three-quarter flex-col mx-auto gap-8">
             <Suspense>
                 <Await resolve={posts} children={children} />
             </Suspense>
+            { infinite && <aside ref={next} /> }
         </div>
     )
 }
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,13 +1,12 @@
-import { useMemo, useCallback, useState, Suspense } from "react";
+import { useMemo, useCallback, useState, useEffect, Suspense } from "react";
 import { useLocation, useNavigate, useParams, Outlet, Await } from 'react-router-dom';
 
 import ProjectCard from './ProjectCard';
-import config from "../../config/github"
-// import useGithub from "../../hooks/useGithub";
-// import useMDX from "../../hooks/useMdx";
+import config from "../../config/github";
 import { useInfiniteQuery } from "react-query";
 import { getFiles, getRepoInfo, getMedia } from "../../lib/github"
 import { parse } from "../../lib/mdx";
+import { useInView } from "react-intersection-observer";
 
 const fetchProjects = async (page, limit) => {
     const files = await getFiles(config.user, config.repo, "gh-projects", page, limit);
@@ -30,11 +29,16 @@ const projectsLoader = (queryClient, page = 0, limit = 9) => async () => {
 }
 
 const Projects = ({ limit = 9, infinite }) => {
-    const { data:projects } = useInfiniteQuery(["projects"], ({pageParam = 0}) => fetchProjects(pageParam, limit), {
+    const {  
+        isFetchingNextPage, 
+        fetchNextPage,
+        data:projects 
+    } = useInfiniteQuery(["projects"], ({pageParam = 0}) => fetchProjects(pageParam, limit), {
         getNextPageParam: (lastPage) =>  lastPage.pages?.next ?? undefined,
         getPreviousPageParam: (firstPage) => firstPage.pages?.prev ?? undefined, 
     })
 
+    const { ref: next, inView: loadNext } = useInView();
     const { pathname } = useLocation();
     const { slug } = useParams();
     const navigate = useNavigate();
@@ -57,11 +61,16 @@ const Projects = ({ limit = 9, infinite }) => {
         }))
     }, [maximize, projects?.pages])
 
+    useEffect(() => {
+        if (loadNext && !isFetchingNextPage) fetchNextPage();
+    }, [loadNext, isFetchingNextPage, fetchNextPage])
+
     return (
         <div className="w-three-quarter mx-auto grid grid-cols grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 items-center justify-center gap-8">
             <Suspense>
                 <Await resolve={projects} children={children} />
             </Suspense>
+            { infinite && <aside ref={next} /> }
             <Outlet context={{ project, setProject }}/>
         </div>
     )
