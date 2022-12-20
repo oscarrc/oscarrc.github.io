30b95a5243d6586ed1e89a99314e569a3366149d
Oscar <oscarrc.web@gmail.com>
2022-12-20 09:13:53 +0100

    [ADD] Code highlighting

--- src/lib/mdx.js
+++ src/lib/mdx.js
@@ -1,12 +1,16 @@
 import * as runtime from "react/jsx-runtime";
 
 import { evaluate } from "@mdx-js/mdx";
+import rehypeHighlight from 'rehype-highlight';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
-
 const parse = async (data) => {
-    const evaluated = await evaluate(data, { ...runtime, remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter] });
+    const evaluated = await evaluate(data, { 
+        ...runtime, 
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter], 
+        rehypePlugins: [[rehypeHighlight, {detect: true}]] 
+    });
     return evaluated;
 }
 
