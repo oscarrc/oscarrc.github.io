cdde160a38265e879dc240c56b471e5678320cdf
Oscar <oscarrc.web@gmail.com>
2022-12-20 14:22:20 +0100

    [WIP] Page transitions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { ScrollRestoration, useLocation } from "react-router-dom";
 import { Suspense, useEffect } from "react";
+import { pageTransition, pageVariants } from "./config/animation";
 
 import Layout from "./components/layout";
 import Loading from "./views/Loading";
 import { Outlet } from "react-router-dom";
 import ReactGA from 'react-ga';
+import { motion } from "framer-motion"
 
 const App = () => {
   const { pathname } = useLocation();
@@ -17,7 +19,15 @@ const App = () => {
   return (
       <Layout>
         <Suspense fallback={<Loading />}>
-          <Outlet />
+          <motion.div
+             key={pathname}
+             initial="initial"
+             animate="in"
+             variants={pageVariants}
+             transition={pageTransition}
+          >
+            <Outlet />
+          </motion.div>
           <ScrollRestoration />
         </Suspense>
       </Layout>
