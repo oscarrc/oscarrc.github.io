0c6dbf3b54d5b1016e172b753a73f0cdf8b821b5
Oscar <oscarrc.web@gmail.com>
2022-12-20 11:45:43 +0100

    [MOD] Landing text

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -30,8 +30,8 @@ const Landing = () => {
             <Me className="lg:max-w-quarter hidden lg:block" fill="fill-secondary" />
             <div className="flex flex-col relative gap-4">
               <h2 className="text-2xl lg:text-3xl font-bold py-2">So you want to know more about me, uh?</h2>
-              <p>Well, I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!</p>
-              <p>Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.</p>
+              <p>Well, I enjoy creating things that live on the internet, but I also enjoy solving my own problems with programming. This interest started back in 2012 when I started a Wordpress blog to earn some pasive income through adSense for which I coded my own theme.</p>
+              <p>Fast-forward to today, and I’ve had the privilege of working at an elearning company, a crypto start-up, a legal corporation, and as a freelancer. My main focus these days is building accessible, and usable products to try and make people life easier.</p>
               <p>Here are a few technologies I’ve been working with recently:</p>
               <ul className="ml-8 grid grid-cols-2 overflow-hidden">
                 <li>&gt; JavaScript (ES6+)</li>
