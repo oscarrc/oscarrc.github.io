ae1a970c09b4d3f40db017ae429a512c232c07cd
Oscar <oscarrc.web@gmail.com>
2022-12-21 13:08:20 +0100

    [FIX] Go back error when no content

--- src/views/Post.js
+++ src/views/Post.js
@@ -7,7 +7,7 @@ import { useMemo } from "react";
 
 const Post = () => {
     const post = useLoaderData();
-    const Content = useMemo(() =>  post?.default, [post.default])
+    const Content = useMemo(() =>  post?.default, [post?.default])
 
     return (
         <Suspense>
@@ -16,16 +16,16 @@ const Post = () => {
                     <div className="container">
                         <div className="prose mx-auto w-three-quarter max-w-three-quarter flex flex-col">
                             <div className="flex ml-auto">
-                                <span className="flex items-center gap-1"><AiOutlineCalendar /> { post.date }</span>
+                                <span className="flex items-center gap-1"><AiOutlineCalendar /> { post?.date }</span>
                                 <span className="divider divider-horizontal mx-0"></span>                    
-                                <span className="flex items-center gap-1"><IoIosTimer /> {post.readingTime} min</span>   
+                                <span className="flex items-center gap-1"><IoIosTimer /> {post?.readingTime} min</span>   
                             </div>
-                            <h2>{post.title}</h2>
-                            <p>{post.excerpt}</p>
+                            <h2>{post?.title}</h2>
+                            <p>{post?.excerpt}</p>
                             <figure className="w-full overflow-hidden mb-4">
-                                <img src={post.image} alt={post.title} />
+                                <img src={post?.image} alt={post?.title} />
                             </figure>
-                            <Content />
+                            { Content && <Content /> }
                         </div>
                     </div>
                 </section>
