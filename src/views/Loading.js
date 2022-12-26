66b13505eec3988f5a90ef1ac8bbe52589a42a53
Oscar <oscarrc.web@gmail.com>
2022-12-26 10:37:31 +0100

    [MOD] Loading size

--- src/views/Loading.js
+++ src/views/Loading.js
@@ -5,7 +5,7 @@ const Loading = () => {
     const { typewritter } = useTypewriter(["..."], 100, 20);
 
     return (
-        <section id="loader" className="flex flex-col justify-center items-center min-h-view">
+        <section id="loader" className="flex flex-col justify-center items-center">
             <div className="hero container relative xl:mt-0 -mt-4">
                 <div className="hero-content min-h-three-quarter w-full flex-col gap-16">
                     <div className="self-center flex flex-col relative text-center xl:text-left">
