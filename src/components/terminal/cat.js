6a2f331dc5fd711e62a936e82b6c66bebd801756
Oscar <oscarrc.web@gmail.com>
2022-12-27 11:57:04 +0100

    [MOD] Cat to use working dir

--- src/components/terminal/cat.js
+++ src/components/terminal/cat.js
@@ -10,24 +10,25 @@ const project = async (queryClient, dir, options) => {
     ];
 
     const notFound = [
-        { text: `File on path ${options[0]} does not exist.`},
+        { text: `File on ${dir ? `${dir}/` : ''}${options[0]} does not exist.`},
         { text: <>Use '<em>ls</em>' to get a list of available files and paths.</>}
     ]
     
     const parseFile = async (path, filename) => {
-        console.log(path, filename)
         const file = await queryClient.fetchQuery([path, filename], () => getFile(config.user, config.repo, `gh-${path}`, filename));
         if(!file || file.includes("404")) return notFound;
         return file.split("\n").map(l => ({ text: l }))
     }
 
-    if(!options.length || !options) return [{ text: "No file was specified"}]
+    if(!options.length) return [{ text: "No file was specified"}]
     if(options.length === 1 && options[0] === "-h") return help;
 
     if(options.length === 1 && options[0][0] !== "-"){
         const opt = options[0].split("/");
-        if(opt.length !== 2) return [{ text: "No such file exists"}]
-        else return await parseFile(opt[0], opt[1]);
+        const path = opt.length === 2 ? opt[0] : dir;
+        const filename = opt.length ===  2 ? opt[1] : opt[0];
+        if(opt.length !== 2 && !dir) return [{ text: "No such file exists"}]
+        else return await parseFile(path, filename);
     }
 
     if(options.length > 1){
