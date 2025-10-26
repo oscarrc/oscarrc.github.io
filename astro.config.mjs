d44faab9c5e28b9cb60183e20e87000985a63b44
Oscar <oscarrc.web@gmail.com>
2025-10-26 15:47:05 +0100

    [feat]: add reading time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkReadingTime from './src/plugins/remark-reading-time';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -16,6 +17,10 @@ export default defineConfig({
   },
   image: {
     responsiveStyles: true,
+  },
+  prefetch: true,
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
   },
   integrations: [
     sitemap(), 
