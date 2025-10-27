1adfe10e76b29eb4e4871c5a3e051d412e6c7a96
Oscar <oscarrc.web@gmail.com>
2025-10-27 20:20:45 +0100

    [feat] posts and projects list sidebar with badges in badgesCard

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -11,3 +11,31 @@ export async function getSortedCollection(collection: "posts" | "projects") {
         new Date(a.data.updated || a.data.published).getTime(),
     )
 }
+
+export async function getSortedTags(collections: Array<"posts" | "projects">) {
+  const tagsMap = new Map<string, number>();
+  for (const collection of collections) {
+    const items = await getCollection(collection);
+    
+    items.forEach(item => {
+      item.data.tags?.forEach(tag => tagsMap.set(tag, (tagsMap.get(tag) || 0) + 1));
+    });
+  }
+
+  const sortedTags = Array.from(tagsMap).sort((a, b) => a[1] - b[1]);
+  return sortedTags;
+}
+
+export async function getSortedSeries(collection: "posts") {
+  const seriesMap = new Map<string, number>();
+  const items = await getCollection(collection);
+
+  items.forEach(item => {
+    if (!item.data.series) return;
+    const series = item.data.series;
+    seriesMap.set(series, (seriesMap.get(series) || 0) + 1);
+  });
+
+  const sortedSeries = Array.from(seriesMap).sort((a, b) => a[1] - b[1]);
+  return sortedSeries;
+}
