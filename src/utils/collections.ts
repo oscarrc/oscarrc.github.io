a38bb67fc972bf7d3dae704e4af17b04c34aeb79
Oscar <oscarrc.web@gmail.com>
2025-11-02 14:20:43 +0100

    [feat]: add tags and series pages

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,10 +1,38 @@
 import { getCollection, type CollectionEntry } from "astro:content"
+import { slug } from "github-slugger"
 
 export async function getSortedCollection(collection: "posts"): Promise<CollectionEntry<"posts">[]>
 export async function getSortedCollection(collection: "projects"): Promise<CollectionEntry<"projects">[]>
 export async function getSortedCollection(collection: "posts" | "projects") {
   return (await getCollection(collection))
     .filter((item) => !item.data.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.data.updated || b.data.published).getTime() -
+        new Date(a.data.updated || a.data.published).getTime(),
+    )
+}
+
+export async function getSortedCollectionByTag(
+  collection: "posts",
+  tagSlug: string
+): Promise<CollectionEntry<"posts">[]>
+export async function getSortedCollectionByTag(
+  collection: "projects",
+  tagSlug: string
+): Promise<CollectionEntry<"projects">[]>
+export async function getSortedCollectionByTag(
+  collection: "posts" | "projects",
+  tagSlug: string
+) {
+  const items = await getCollection(collection);
+  
+  return items
+    .filter((item) => {
+      if (item.data.draft) return false;
+      if (!item.data.tags) return false;
+      return item.data.tags.some(tag => slug(tag) === tagSlug);
+    })
     .sort(
       (a, b) =>
         new Date(b.data.updated || b.data.published).getTime() -
@@ -42,4 +70,23 @@ export async function getSortedSeries(collection: "posts") {
 
   const sortedSeries = Array.from(seriesMap).sort((a, b) => b[1] - a[1]);
   return sortedSeries;
+}
+
+export async function getSortedCollectionBySeries(
+  collection: "posts",
+  seriesSlug: string
+): Promise<CollectionEntry<"posts">[]> {
+  const items = await getCollection(collection);
+  
+  return items
+    .filter((item) => {
+      if (item.data.draft) return false;
+      if (!item.data.series) return false;
+      return slug(item.data.series) === seriesSlug;
+    })
+    .sort(
+      (a, b) =>
+        new Date(b.data.updated || b.data.published).getTime() -
+        new Date(a.data.updated || a.data.published).getTime(),
+    )
 }
