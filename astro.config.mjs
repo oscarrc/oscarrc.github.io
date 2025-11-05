30e9e2a56161a9b8a732698c4cc919e5edc4a9b7
Oscar <oscarrc.web@gmail.com>
2025-11-05 18:35:20 +0100

    [feat]: add expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
-import remarkReadingTime from './src/plugins/remark-reading-time';
-import remarkFrontmatterMeta from './src/plugins/remark-frontmatter-meta';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import rehypeHeadingHash from './src/plugins/rehype-heading-hash';
 import rehypeHrDashes from './src/plugins/rehype-hr-dashes';
 import rehypeListMarkers from './src/plugins/rehype-list-markers';
+import remarkFrontmatterMeta from './src/plugins/remark-frontmatter-meta';
+import remarkReadingTime from './src/plugins/remark-reading-time';
 import siteConfig from './src/site.config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
@@ -30,7 +32,15 @@ export default defineConfig({
     rehypePlugins: [rehypeHeadingHash, rehypeHrDashes, rehypeListMarkers],
   },
   integrations: [
-    sitemap(), 
+    sitemap(),    
+    expressiveCode({
+      themes: ['catppuccin-mocha'],
+      useDarkModeMediaQuery: false,
+      defaultProps: {
+        wrap: false,
+      },
+      plugins: [pluginLineNumbers()]
+    }),
     mdx()
   ]
 });
